31e899ca041a2d4648364b5eaf0221c178ea2c8c
ezy4me <mrronal@mail.ru>
2024-06-14 13:44:42 +0300

    added a profile and changed styles

---
+++ src/components/Profile.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaTelegram } from "react-icons/fa";
+import Typewriter from "./Typewriter";
+
+const Profile: React.FC = () => {
+  const texts = [
+    "Frontend Developer",
+    "Tech Enthusiast",
+    "Lover of Music",
+    "Always Learning New Things",
+  ];
+
+  return (
+    <div className="profile">
+      <div className="profile__header">
+        <p>Profile Header</p>
+      </div>
+      <div className="profile__content">
+        <div className="profile__image">
+          <img src="/public/avatar.png" alt="profile" />
+        </div>
+        <div className="profile__body">
+          <p className="profile__name">Maximov Roman</p>
+          <Typewriter texts={texts} speed={100} />
+          <div className="profile__socials">
+            <a href="#" className="profile__social-link">
+              <FaTelegram />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,47 +1,6 @@
-import { FaHome, FaUser, FaMailBulk, FaBook } from "react-icons/fa";
-import { Link, Outlet, useLocation } from "react-router-dom";
-
-const Sidebar = () => {
-  const location = useLocation();
-  const currentPath = location.pathname;
-
-  return (
-    <div className="sidebar">
-      <Link
-        to="/"
-        className={`sidebar__item ${
-          currentPath === "/" ? "sidebar__item--active" : ""
-        }`}>
-        <FaHome className="sidebar__icon" />
-        <span className="sidebar__text">Home</span>
-      </Link>
-      <Link
-        to="/about"
-        className={`sidebar__item ${
-          currentPath === "/about" ? "sidebar__item--active" : ""
-        }`}>
-        <FaUser className="sidebar__icon" />
-        <span className="sidebar__text">About</span>
-      </Link>
-      <Link
-        to="/contact"
-        className={`sidebar__item ${
-          currentPath === "/contact" ? "sidebar__item--active" : ""
-        }`}>
-        <FaMailBulk className="sidebar__icon" />
-        <span className="sidebar__text">Contact</span>
-      </Link>
-      <Link
-        to="/projects"
-        className={`sidebar__item ${
-          currentPath === "/projects" ? "sidebar__item--active" : ""
-        }`}>
-        <FaBook className="sidebar__icon" />
-        <span className="sidebar__text">Projects</span>
-      </Link>
-    </div>
-  );
-};
+import Profile from "@/components/Profile";
+import Sidebar from "@/components/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const MainContent = () => {
   return (
@@ -56,6 +15,7 @@ const MainLayout = () => {
     <div className="container">
       <div className="layout">
         <Sidebar />
+        <Profile />
         <div className="layout__content">
           <div className="layout__header">
             <h1 className="layout__title">My Portfolio</h1>

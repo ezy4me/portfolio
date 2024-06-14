621be81963729c52196a9a9801f28f09e9908ecd
ezy4me <mrronal@mail.ru>
2024-06-14 21:08:39 +0300

    added contact form

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -2,9 +2,52 @@ function ContactPage() {
   return (
     <div className="page contact-page">
       <div className="page__header">
-        <h1 className="page__title">// Contacts</h1>
+        <h1 className="page__title">// Contact Me</h1>
       </div>
-      <div className="page__content"></div>
+      <div className="page__content">
+        <form className="contact-form">
+          <div className="contact-form__group">
+            <label htmlFor="name" className="contact-form__label">
+              Name:
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="contact-form__input"
+              placeholder="Your name"
+              required
+            />
+          </div>
+          <div className="contact-form__group">
+            <label htmlFor="email" className="contact-form__label">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your email"
+              className="contact-form__input"
+              required
+            />
+          </div>
+          <div className="contact-form__group">
+            <label htmlFor="message" className="contact-form__label">
+              Message:
+            </label>
+            <textarea
+              id="message"
+              placeholder="Your message"
+              name="message"
+              className="contact-form__textarea"
+              required></textarea>
+          </div>
+          <button type="submit" className="btn contact-form__button">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
